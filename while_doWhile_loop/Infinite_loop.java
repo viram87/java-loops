a14393571a9e061171ebb3ceca30afc33102dd97
viram choksi <viramchoksi77166@gmail.com>
2021-04-01 09:51:14 +0530

    i added syntax code for while and do while loop

---
+++ while_doWhile_loop/Infinite_loop.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class Infinite_loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 1;
+		
+		while(true)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+
+}
---
+++ while_doWhile_loop/While_loop.java
@@ -0,0 +1,37 @@
+//while loop
+
+package loops;
+
+public class While_loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//case 1
+		System.out.println("case 1");
+		int i = 1;
+		
+		while(i<100)
+		{
+			 System.out.println(i);
+			 i = i * 2;
+		}
+		
+		System.out.println(" ");
+		
+		//case 2
+		System.out.println("case 2");
+		
+		int j = 100;
+		
+		while(j<100) 
+		{
+			System.out.println(j);
+			j = j *2;
+		}
+	
+	
+	}
+	
+
+}

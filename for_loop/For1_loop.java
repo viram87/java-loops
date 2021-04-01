cf7a8c82503a54449f95f21cf272c172e8790b6b
viram choksi <viramchoksi77166@gmail.com>
2021-04-01 09:57:25 +0530

    im too lazy in commneting

---
+++ for_loop/For1_loop.java
@@ -0,0 +1,17 @@
+package for_loop;
+
+public class For1_loop {
+
+	public static void main(String[] args) {
+
+		for(int i = 1 ; i<= 10 ; i++ )
+		{
+			System.out.println(i);
+		}
+		
+	}
+
+}
+
+
+

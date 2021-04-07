ee8188b4d614d0f8a3921658518b85651f8d5655
viram choksi <viramchoksi77166@gmail.com>
2021-04-07 16:40:04 +0530

    add new pattern type program in nested loop

---
+++ Patterns/pattern3.java
@@ -0,0 +1,41 @@
+/* draw pattern
+
+01 02 03 04 05  
+06 07 08 09 10
+11 12 13 14 15
+16 17 18 19 20  
+21 22 23 24 25
+
+*/
+
+package Patterns;
+
+import java.util.Scanner;
+
+public class pattern3 {
+
+    public static void main(String[] args) {
+
+        Scanner v1 = new Scanner(System.in);
+
+        System.out.println("Enter the number : ");
+        int n = v1.nextInt();
+
+        System.out.println(" ");
+
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                count++;
+                System.out.format("%02d ", count);
+            }
+            System.out.println(" ");
+
+        }
+
+        v1.close();
+
+    }
+
+}

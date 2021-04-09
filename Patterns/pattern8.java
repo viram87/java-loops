406e6bb0c80e2ef67de0710472b4b669f973ca1d
viram choksi <viramchoksi77166@gmail.com>
2021-04-09 15:12:57 +0530

    add some new patterns

--- Patterns/pattern8.java
+++ Patterns/pattern8.java
@@ -1,8 +1,44 @@
+/* Draw the pattern
+
+* * * * *  
+  * * * *
+    * * *
+      * *
+        *  
+
+*/
+
 package Patterns;
+
+import java.util.Scanner;
 
 public class pattern8 {
 
     public static void main(String[] args) {
+
+        Scanner v1 = new Scanner(System.in);
+
+        System.out.println("Enter the number : ");
+
+        int n = v1.nextInt();
+
+        System.out.println(" ");
+
+        for (int i = 1; i <= n; i++) {
+            // for spaces
+            for (int j = 1; j <= i - 1; j++) {
+                System.out.print("  ");
+            }
+
+            // for stars
+            for (int k = 0; k <= n - i; k++) {
+                System.out.print("* ");
+            }
+
+            System.out.println(" ");
+        }
+
+        v1.close();
 
     }
 

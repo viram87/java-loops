05b6e75828e10e5ffd1413f04fcb02687708b39b
viram choksi <viramchoksi77166@gmail.com>
2021-04-09 16:44:02 +0530

    add new patterns

--- Patterns/pattern10.java
+++ Patterns/pattern10.java
@@ -1,5 +1,33 @@
 package Patterns;
 
+import java.util.Scanner;
+
 public class pattern10 {
+
+    public static void main(String[] args) {
+
+        Scanner v1 = new Scanner(System.in);
+
+        System.out.println("Enter the number : ");
+
+        int n = v1.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            // for spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+
+            // for stars
+            for (int k = 1; k <= (2 * i) - 1; k++) {
+                System.out.print("* ");
+            }
+
+            System.out.println(" ");
+        }
+
+        v1.close();
+
+    }
 
 }
